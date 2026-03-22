3d068729a79a8deafad4150ec70772e4f437b18e
hasanujjamanjibon <hasanujjamanjibon2@gmail.com>
2026-03-22 19:24:13 +0600

    contribute button unusal margin removed

--- src/components/BtnContribute.jsx
+++ src/components/BtnContribute.jsx
@@ -1,16 +1,16 @@
 import { RiGithubFill } from '@remixicon/react';
+import { Link } from 'react-router';
 
 const BtnContribute = () => {
   return (
-    <a
-      href='https://github.com/hasanujjamanjibon'
+    <Link
+      to='https://github.com/hasanujjamanjibon'
       target='_blank'
-      rel='noopener noreferrer'
-      className='text-white bg-linear-to-br from-purple-900 via-purple-800 to-purple-700 hover:bg-linear-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 flex items-center gap-1 cursor-pointer opacity-75 hover:opacity-100 transition-opacity duration-300 w-fit'
+      className='text-white bg-linear-to-br from-purple-900 via-purple-800 to-purple-700 hover:bg-linear-to-br  font-medium rounded-lg text-sm px-5 py-2.5 text-center  flex items-center gap-1 cursor-pointer opacity-75 hover:opacity-100 transition-opacity duration-300 w-fit'
     >
       <RiGithubFill color='#ffffff' />
       Contribute
-    </a>
+    </Link>
   );
 };
 
